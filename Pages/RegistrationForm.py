81345d7d0cb2f4f811ae4200bf9952adc6af49c4
Animesh <er.animesh6@hotmail.com>
2021-06-26 11:15:37 +0530

    config.ini file added for regsitration page

--- Pages/RegistrationForm.py
+++ Pages/RegistrationForm.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 from Utilities import configReader
 
 
@@ -10,7 +12,9 @@ class RegistrationPage:
         self.driver.find_element(By.XPATH, configReader.readConfig("", "")).send_keys(name)
         self.driver.find_element(By.XPATH, configReader.readConfig("", "")).send_keys(ph_no)
         self.driver.find_element(By.XPATH, configReader.readConfig("", "")).send_keys(email)
-        self.driver.find_element(By.XPATH, configReader.readConfig("", "")).send_keys(name)
+        country_dropdown = self.driver.find_element(By.XPATH, configReader.readConfig("", ""))
+        select_country = Select(country_dropdown)
+        select_country.select_by_visible_text(country)
         self.driver.find_element(By.XPATH, configReader.readConfig("", "")).send_keys(city)
         self.driver.find_element(By.XPATH, configReader.readConfig("", "")).send_keys(username)
         self.driver.find_element(By.XPATH, configReader.readConfig("", "")).send_keys(password)
