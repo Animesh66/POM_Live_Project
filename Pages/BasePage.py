d89f18ca0b41b834238bb1a4b75e54c0facec952
Animesh <er.animesh6@hotmail.com>
2021-06-29 20:56:17 +0530

    config.ini file added for regsitration page

--- Pages/BasePage.py
+++ Pages/BasePage.py
@@ -1,7 +1,21 @@
+from selenium.webdriver.common.by import By
+from Utilities import configReader
+
+
 class BasePage:
 
     def __init__(self, driver):
         self.driver = driver
 
+    def click(self, locator):  # this function will click on the locator
+        if str(locator).endswith("_xpath"):
+            self.driver.find_element(By.XPATH(configReader.readConfig("locator", locator)).click())
+        elif str(locator).endswith("_css"):
+            self.driver.find_element(By.CSS_SELECTOR(configReader.readConfig(("locator", locator)).click()))
+        elif str(locator).endswith("_id"):
+            self.driver.find_element(By.ID(configReader.readConfig(("locator", locator)).click()))
+
+    def type(self, locator, value):
+        self.driver.find_element(By.XPATh(configReader.readConfig("locator", locator)).send_keys(value))
 
 
