381da3df0d8fb63e3521b692c02baed8ccce9d7b
Animesh <er.animesh6@hotmail.com>
2021-06-29 21:11:05 +0530

    config.ini file added for registration page

--- Pages/BasePage.py
+++ Pages/BasePage.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
 from Utilities import configReader
 
 
@@ -8,14 +10,15 @@ class BasePage:
         self.driver = driver
 
     def click(self, locator):  # this function will click on the locator
-        if str(locator).endswith("_xpath"):  # if the locator variable name is ends with _xpath it will click on the xpath
+        if str(locator).endswith("_xpath"):  # if the locator variable name is ends with _xpath it will click on the
+            # xpath
             self.driver.find_element(By.XPATH(configReader.readConfig("locator", locator)).click())
         elif str(locator).endswith("_css"):
             self.driver.find_element(By.CSS_SELECTOR(configReader.readConfig(("locator", locator)).click()))
         elif str(locator).endswith("_id"):
             self.driver.find_element(By.ID(configReader.readConfig(("locator", locator)).click()))
 
-    def type(self, locator, value):
+    def type(self, locator, value):  # this is the replacement method of send_keys
         if str(locator).endswith("_xpath"):
             self.driver.find_element(By.XPATH(configReader.readConfig("locator", locator)).send_keys(value))
         elif str(locator).endswith("_css"):
@@ -23,4 +26,14 @@ class BasePage:
         elif str(locator).endswith("_id"):
             self.driver.find_element(By.ID(configReader.readConfig(("locator", locator)).send_keys(value)))
 
+    def select_dropdown(self, locator, value):  # this is for dropdown value selection method
+        global dropdown
+        if str(locator).endswith("_xpath"):
+            dropdown = self.driver.find_element(By.XPATH(configReader.readConfig("locator", locator)).send_keys(value))
+        elif str(locator).endswith("_css"):
+            dropdown = self.driver.find_element(By.CSS_SELECTOR(configReader.readConfig(("locator", locator)).send_keys(value)))
+        elif str(locator).endswith("_id"):
+            dropdown = self.driver.find_element(By.ID(configReader.readConfig(("locator", locator)).send_keys(value)))
+        select = Select(dropdown)
+        select.select_by_visible_text(value)
 
