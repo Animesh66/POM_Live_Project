6f2dde8407bdeb382b75cfe729e65ebbbe253ba7
Animesh <er.animesh6@hotmail.com>
2021-06-29 19:03:25 +0530

    config.ini file added for regsitration page

--- Pages/BasePage.py
+++ Pages/BasePage.py
@@ -2,3 +2,6 @@ class BasePage:
 
     def __init__(self, driver):
         self.driver = driver
+
+
+
---
+++ Pages/LoginPage.py
@@ -0,0 +1,6 @@
+class LoginPage:
+
+    def __init__(self, driver):
+        super().__init__(driver)
+
+    def login(self):
